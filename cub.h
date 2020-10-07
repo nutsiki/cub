afbc0f269f259f93894961923f8f8252342a9a09
moakouda <mo.akoudad@gmail.com>
2020-10-07 17:43:20 +0200

    nikesamere

--- cub.h
+++ cub.h
@@ -1,17 +1,41 @@
 #ifndef CUB3D_H
 # define CUB3D_H
 # define SIZE_MAP 10000
+# define CHECK_R 1
+# define CHECK_NO 2
+# define CHECK_SO 4
+# define CHECK_WE 8
+# define CHECK_EA 16
+# define CHECK_S 32
+# define CHECK_F 64
+# define CHECK_C 128
 
 #include <fcntl.h>
 #include <string.h>
 //#include "Libft/libft.h"
+
+typedef struct s_txt
+{
+	char *txt_no;
+	char *txt_so;
+	char *txt_we;
+	char *txt_ea;
+	char *txt_s;
+}				t_txt;
 
 typedef struct	s_map
 {
 	char *line;
 	int index;
 	int size;
+	int reso_x;
+	int reso_y;
+	char *txt_no;
+	char *txt_so;
+	char *txt_we;
+	char **txt;
 }				t_map;
+
 
 int 	check_map(char c);
 //int 	parser_map(char *map, int size, map);
--- main.c
+++ main.c
@@ -1,12 +1,95 @@
 #include "cub.h"
 #include <stdio.h>
 
+size_t	ft_strlen(const char *s)
+{
+	size_t i;
+
+	i = 0;
+	while (*s++)
+		i++;
+	return (i);
+}
+
 int		check_is_space(char str)
 {
 	if (str == '\f' || str == '\r'
-		   || str == '\t' || str == '\v' || str == ' ')
+		|| str == '\t' || str == '\v' || str == ' ' || str == '\n' || str == '\0')
 		return (1);
 	return (0);
+}
+
+static char				*ft_skipspace(char *str)
+{
+	while (check_is_space(*str))
+		str++;
+	return (str);
+}
+
+static char				*ft_checkspace(char *str)
+{
+	while (*str == '\n' || *str == '\f' || *str == '\r'
+		   || *str == '\t' || *str == '\v' || *str == ' ')
+		str++;
+	return (str);
+}
+
+static int				ft_checkull(const char *str, int j)
+{
+	int					i;
+	char				*tab;
+	int					k;
+	int					m;
+
+	m = 0;
+	tab = "18446744073709551615";
+	i = ft_strlen(str);
+	k = ft_strlen(tab);
+	if (i < k)
+		return (1);
+	while (m++ < i)
+	{
+		if (!(str[m] >= '0' && str[m] <= '9'))
+			return (1);
+		if (str[m] > tab[m] && j == 1)
+			return (-1);
+		if (str[m] > tab[m] && j == -1)
+			return (0);
+	}
+	return (1);
+}
+
+int						ft_atoi(const char *str)
+{
+	int					j;
+	int					k;
+	unsigned long long	nb;
+
+	j = 1;
+	nb = 0;
+	str = ft_checkspace((char*)str);
+	if (*str == '-')
+		j = -1;
+	if (*str == '+' || *str == '-')
+		str++;
+	if ((k = ft_checkull(str, j)) != 1)
+		return (k);
+	while (*str && *str >= '0' && *str <= '9')
+	{
+		nb = (nb * 10) + (*str - '0');
+		if (nb > 9223372036854775807 && j == 1)
+			return (-1);
+		if (nb > 9223372036854775807 && j == -1)
+			return (0);
+		str++;
+	}
+	return ((int)(nb * j));
+}
+
+int		ft_isalnum(int c)
+{
+	return ((c >= 97) && (c <= 122)) || ((c >= 65) && (c <= 90)) ||
+		   ((c >= 48) && (c <= 57));
 }
 
 int		is_cardinal(char c)
@@ -63,6 +146,126 @@ int ft_realloc(char **tab, int elem_size, int nb_elem, int new_size)
 	return (new_size);
 }
 
+char 	**init_tab(void)
+{
+	char **tab;
+	int i;
+
+	i = -1;
+	tab = (char**)malloc(sizeof(char*)*6);
+	while (++i < 5)
+		tab[i] = calloc(sizeof(char),3);
+	tab[i] = "\0";
+	tab[0] = "NO";
+	tab[1] = "SO";
+	tab[2] = "WE";
+	tab[3] = "EA";
+	tab[4] = "S ";
+	return (tab);
+}
+
+int 	check_txt(char a, char b, char **tab)
+{
+	int i;
+
+	i = -1;
+	while (++i < 5)
+	{
+		if (tab[i][0] == a && tab[i][1] == b)
+			return (i + 1);
+	}
+	return (0);
+}
+
+int 	check_no(char *buf, int size, t_map **map)
+{
+	int i;
+	int t;
+	char **tab;
+	int l;
+
+	i = 0;
+	l = 0;
+	buf = ft_skipspace(buf);
+	tab = init_tab();
+	(*map)->txt=(char**)malloc(sizeof(char)*6);
+//	while (tab++)
+//	{
+//		*tab = malloc(sizeof(char)*3);
+//		*tab[2] = '\0';
+//	}
+//	tab = {"NO", "SO", "WE", "EA", "S "};
+	while ((t = check_txt(*buf, *(buf+1), tab)))
+	{
+		if (l == t)
+			return (0);
+		l = t;
+		printf("t vaut %d\n", t);
+		buf = buf + 2;
+		(*map)->txt[t - 1] = (char*)calloc(sizeof(char),size);
+		buf = ft_skipspace(buf);
+		printf("cou [%c] \n", *buf);
+		printf("0 add vaut [%p] \n", (*map)->txt[t - 1]);
+		i = 0;
+		while (!(check_is_space(*(buf))))
+		{
+			printf("buf vaut %c\n", *buf);
+			(*map)->txt[t - 1][i++] = *buf++;
+			printf("txt vaut %c\n", (*map)->txt[t - 1][i - 1]);
+		}
+		buf = ft_skipspace(buf);
+
+		printf("string vaut [%s] \n", (*map)->txt[t - 1]);
+		printf("0 add vaut [%p] \n", (*map)->txt[t - 1]);
+	}
+	return (1);
+}
+
+char 	*check_r(char *buf, int size, t_map **map)
+{
+	char *buf_temp;
+	int i;
+
+	i = 0;
+	buf = ft_skipspace(buf);
+	if (*buf++ == 'R')
+	{
+		buf_temp = (char*)calloc(sizeof(char),size);
+		buf = ft_skipspace(buf);
+		printf("1 buf vaut %c\n", *buf);
+		while (ft_isalnum(*(buf)))
+		{
+			printf("2 buf_temp vaut %c\n", *buf_temp);
+			printf("3 buf vaut %c\n", *buf);
+			buf_temp[i++] = (*buf++);
+			printf("2a buf_temp vaut %c\n", *buf_temp);
+			printf("3a buf vaut %c\n", *buf);
+		}
+		printf("4 buf vaut %s\n", buf_temp);
+		(*map)->reso_x = ft_atoi(buf_temp);
+		printf("5 buf vaut %c\n", *buf);
+		i = 0;
+		buf = ft_skipspace(buf);
+		printf("6 buf vaut %c\n", *buf);
+
+		while (ft_isalnum(*buf))
+			buf_temp[i++] = *buf++;
+		(*map)->reso_y = ft_atoi(buf_temp);
+	}
+	else
+		return (0);
+	free((void*)buf_temp);
+	return (buf);
+}
+
+int 	parser_premap(char *buf, int size, t_map **map)
+{
+	buf = check_r(buf, size, map);
+	printf("yo [%c]\n", *buf);
+	check_no(buf, size, map);
+	return (1);
+}
+
 int 	parser_map(char *buffer, int size, t_map **map)
 {
 	int k;
@@ -73,6 +276,7 @@ int 	parser_map(char *buffer, int size, t_map **map)
 	k = 2;
 	x = 0;
 	size_map = 20;
+	printf("nivo\n");
 	while (check_map(*buffer) && k)
 	{
 //		printf("couco[%d]\n", x);
@@ -115,24 +319,21 @@ int	flood_fill(t_map **map, int j, int i, int x)
 {
 	printf("0a coucou map[%d]->line[%d] vaut %c\n", j, i, (*map)[j].line[i]);
 	if ((*map)[j].line[i] == 'X')
-	{
-//		(*map)[j].line[i] = '0';
 		return (1);
-	}
 	(*map)[j].line[i] = 'X';
 	printf("1a coucou map[%d]->line[%d] vaut %c\n", j, i, (*map)[j].line[i]);
-	if ((i - 1 >= 0) && (!(check_is_space((*map)[j].line[i - 1]))))
+	if ((i - 1 >= 0) && (!(check_is_space((*map)[j].line[i - 1]))) && (*map)[j].line[i - 1] != '1')
 		flood_fill(map, j, i - 1, x);
 	printf("2a coucou map[%d]->line[%d] vaut %c\n", j, i, (*map)[j].line[i]);
-	if ((i + 1 < (*map)[j].size) && (!(check_is_space((*map)[j].line[i + 1]))))
+	if ((i + 1 < (*map)[j].index) && (!(check_is_space((*map)[j].line[i + 1]))) && (*map)[j].line[i + 1] != '1')
 		flood_fill(map, j, i + 1, x);
 	printf("3a coucou map[%d]->line[%d] vaut %c\n", j, i, (*map)[j].line[i]);
-	if ((j - 1 >= 0) && (!(check_is_space((*map)[j - 1].line[i]))))
+	if ((j - 1 >= 0) && (!(check_is_space((*map)[j - 1].line[i]))) && (*map)[j - 1].line[i] != '1')
 		flood_fill(map, j - 1, i, x);
 	printf("4a coucou map[%d]->line[%d] vaut %c\n", j, i, (*map)[j].line[i]);
-	if ((j + 1 < x) && (!(check_is_space((*map)[j + 1].line[i]))))
+	if ((j + 1 < x) && (!(check_is_space((*map)[j + 1].line[i]))) && ((*map)[j + 1].line[i]) != '1')
 		flood_fill(map, j + 1, i, x);
-	printf("5a coucou map[%d]->line[%d] vaut %c\n", j, i, (*map)[j].line[i]);
+	printf("PAS CORRECT map[%d]->line[%d] vaut %c\n", j, i, (*map)[j].line[i]);
 	return (0);
 }
 
@@ -142,7 +343,6 @@ int check_wall(t_map **map, int x)
 	int j;
 
 	j = -1;
-	printf("test : %s\n", (*map)[2].line);
 	while (++j < x)
 	{
 		i = -1;
@@ -152,14 +352,15 @@ int check_wall(t_map **map, int x)
 			printf("2 map[%d]->line[%d] : %c\n", j, i, (*map)[j].line[i]);
 			if ((*map)[j].line[i] == '0')
 			{
-				flood_fill(map, j, i, x);
+				if(!(flood_fill(map, j, i, x)))
+					return (0);
 			}
 			printf("3 map[%d]->line[%d] : %c\n", j, i, (*map)[j].line[i]);
 		}
 		printf("4 map[%d]->line[%d]\n", j, i);
 
 	}
-	return (0);
+	return (1);
 }
 
 t_map 	*init_map()
@@ -174,6 +375,8 @@ t_map 	*init_map()
 	map->index = 0;
 	map->line = calloc(size_map, sizeof(char));
 	map->line[0] = 0;
+	map->reso_x = 0;
+	map->reso_y = 0;
 	return (map);
 }
 
@@ -185,7 +388,7 @@ int		main(void)
 	int x;
 	t_map *map;
 
-	if ((fd = open("map.txt", O_RDONLY)) == -1)
+	if ((fd = open("map1.txt", O_RDONLY)) == -1)
 		printf("erreur dans le fichier");
 	map = init_map();
 	while ((size = read(fd, buf, SIZE_MAP)) > 0)
@@ -193,15 +396,30 @@ int		main(void)
 //		printf("coucouc %d--%d\n", size, SIZE_MAP);
 
 		buf[size] = '\0';
+
+		parser_premap(buf, size, &map);
+		printf("coucou\n");
+		printf("%d--%d\n", map->reso_x, map->reso_y);
+		printf("txt 0 %s\n", map->txt[0]);
+		printf("txt 1 %s\n", map->txt[1]);
+		printf("txt 2 %s\n", map->txt[2]);
+		printf("txt 3 %s\n", map->txt[3]);
+		printf("txt 4 %s\n", map->txt[4]);
 		if (!(x = parser_map(buf, size, &map)))
 			return (1);
-		check_wall(&map, x);
+		printf("samiiiiiiii0 : %p\n", map[1].line);
+		printf("samiiiiiiii0 : %p\n", &map);
+		printf("samiiiiiiii0 : %p\n", map);
+		if (!(check_wall(&map, x)))
+			return (-1);
 		printf(" x vaut : %d\n", x);
 		int k = -1;
-//		printf("1 : %s\n", map[16].line);
-//		printf("1 : %p\n", &map[0]);
+		printf("1 : %p\n", map[1].line);
+		printf("1 : %p\n", &map);
+		printf("1 : %p\n", map);
+//		printf("1 : %s\n", map[1].line);
 //		printf("2 : %p\n", &map[950]);
-		while (++k < x)
+		while (++k <= x)
 		{
 			printf("%s\n", map[k].line);
 //		map[k].size = ft_realloc(&map[k].line, sizeof(char), map[k].size, max_size);
